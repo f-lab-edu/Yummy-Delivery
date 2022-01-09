f8cd3fd7d03b918c8b28ac7234807dca71bc79f4
이원기 <log19951@gmail.com>
2022-01-09 19:48:05 +0900

    일부 중복 코드 삭제   - 동일 코드에 이름만 다른 클래스(PasswordEncoderConfig.java, WebSecurityConfig.java) 중 PasswordEncoderConfig 클래스 삭제   - domailn 패키지와 domain 패키지 중복으로 domailn 패키지 삭제

--- src/main/java/com/yummy/delivery/config/WebSecurityConfig.java
+++ src/main/java/com/yummy/delivery/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.yummy.delivery.security;
+package com.yummy.delivery.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/yummy/delivery/domain/User.java
+++ src/main/java/com/yummy/delivery/domain/User.java
@@ -1,5 +1,6 @@
 package com.yummy.delivery.domain;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -8,6 +9,7 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@Builder
 @ToString
 public class User {
 
